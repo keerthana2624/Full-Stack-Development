768d25273d942caabd02c55394e383139dc3937f
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-12 14:39:03 +0530

    implimented the Ternary operator

--- Types_operators.js
+++ Types_operators.js
@@ -22,11 +22,11 @@
     console.log(-200 / 0);
     console.log(-50 / 0);         
     console.log(-1e308 * 2); 
-    console.log(-10e308)
+    console.log(-10e308);
 
     // (iii)NaN 
-    console.log("k"*"k")
-    console.log(0 / 0)
+    console.log("k"*"k");
+    console.log(0 / 0);
     console.log("hello" * 2);
     console.log("10" - "five"); 
     console.log("hello" / 3);
@@ -39,11 +39,11 @@
     console.log("Concatenation" + "works!");
 
 //  4.Boolean
-    console.log(5>3)
-    console.log(10==5)
+    console.log(5>3);
+    console.log(10==5);
     console.log(6>4>1)
-    console.log(true||false)
-    console.log(true&&false)
+    console.log(true||false);
+    console.log(true&&false);
 
 // 5.Empty value
     console.log(null < 0);
@@ -53,7 +53,7 @@
     console.log(undefined == 0);
 
 // 6.Unary operator
-    console.log(30)
+    console.log(30);
     console.log("3.14");
     console.log(typeof "hello");
     console.log(!true);
@@ -61,9 +61,19 @@
     console.log(!0);
 
 // 7.Logical operator
+    console.log(5 > 10 && 2 > 1);
+    console.log(3 > 2 || 4 > 5);
+    console.log("banana" === "apple" || 10 !== 10);
+    console.log(true && true);
+    console.log(false || false); 
+    console.log(!false);
 
-
-
+// 8.Ternary
+    console.log(5 > 2?"greater":"smaller" );
+    console.log(5 == 5? "equal":"notequal");
+    console.log(0 === 0?"zero":"notzero");
+    console.log("keerthi" === "keerthana"?"same":"different");
+    console.log(60 <= 50 ? "Below or equal to 50" : "Above 50");
 
    
 
