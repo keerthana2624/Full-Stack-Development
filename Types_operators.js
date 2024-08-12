362d9017c8574afe4f93fac9abcb1bc9f25324c4
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-12 15:16:23 +0530

    did some modifications

--- Types_operators.js
+++ Types_operators.js
@@ -1,6 +1,6 @@
 // Data Types
 // 1.Numbers
-
+    console.log("----------------------");
     console.log(2+5);
     console.log(20 - 30);
     console.log(1.5+2.0);
@@ -11,6 +11,7 @@
 // 2.Special Numbers
 
     // (i)Infinity
+    console.log("----------------------");
     console.log(10 / 0);    
     console.log(1000 / 0); 
     console.log(2e308);     
@@ -18,6 +19,7 @@
     console.log(2e400);
 
     // (ii)Negative Infinity
+    console.log("----------------------");
     console.log(-100 / 0);        
     console.log(-200 / 0);
     console.log(-50 / 0);         
@@ -25,6 +27,7 @@
     console.log(-10e308);
 
     // (iii)NaN 
+    console.log("----------------------");
     console.log("k"*"k");
     console.log(0 / 0);
     console.log("hello" * 2);
@@ -32,6 +35,7 @@
     console.log("hello" / 3);
 
 //  3.String
+    console.log("----------------------");
     console.log("Hello guys");
     console.log('funny program');
     console.log(`In the year ${20001}`);
@@ -39,6 +43,7 @@
     console.log("Concatenation" + "works!");
 
 //  4.Boolean
+    console.log("----------------------");
     console.log(5>3);
     console.log(10==5);
     console.log(6>4>1)
@@ -46,6 +51,7 @@
     console.log(true&&false);
 
 // 5.Empty value
+    console.log("----------------------");
     console.log(null < 0);
     console.log(null == 0); 
     console.log(null * 2);
@@ -53,6 +59,7 @@
     console.log(undefined == 0);
 
 // 6.Unary operator
+    console.log("----------------------");
     console.log(30);
     console.log("3.14");
     console.log(typeof "hello");
@@ -61,6 +68,7 @@
     console.log(!0);
 
 // 7.Logical operator
+    console.log("----------------------");
     console.log(5 > 10 && 2 > 1);
     console.log(3 > 2 || 4 > 5);
     console.log("banana" === "apple" || 10 !== 10);
@@ -69,6 +77,7 @@
     console.log(!false);
 
 // 8.Ternary
+    console.log("----------------------");
     console.log(5 > 2?"greater":"smaller" );
     console.log(5 == 5? "equal":"notequal");
     console.log(0 === 0?"zero":"notzero");
@@ -76,6 +85,7 @@
     console.log(60 <= 50 ? "Below or equal to 50" : "Above 50");
 
 // 9.Automatic type conversion
+    console.log("----------------------");
     console.log("5" + 2);
     console.log(10 + "5"); 
     console.log("5" * "4");
