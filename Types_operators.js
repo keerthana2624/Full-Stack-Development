d9460473c43495c98272a8e002b65710e56ea10f
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-12 14:09:44 +0530

    implimented the Empty value with some examples

--- Types_operators.js
+++ Types_operators.js
@@ -1,5 +1,6 @@
 // Data Types
 // 1.Numbers
+
     console.log(2+5);
     console.log(20 - 30);
     console.log(1.5+2.0);
@@ -8,36 +9,48 @@
     console.log(10 % 3);
     
 // 2.Special Numbers
+
     // (i)Infinity
     console.log(10 / 0);    
     console.log(1000 / 0); 
     console.log(2e308);     
     console.log(1e308 * 10);
     console.log(2e400);
+
     // (ii)Negative Infinity
     console.log(-100 / 0);        
     console.log(-200 / 0);
     console.log(-50 / 0);         
     console.log(-1e308 * 2); 
     console.log(-10e308)
+
     // (iii)NaN 
     console.log("k"*"k")
     console.log(0 / 0)
     console.log("hello" * 2);
     console.log("10" - "five"); 
     console.log("hello" / 3);
+
 //  3.String
     console.log("Hello guys");
     console.log('funny program');
     console.log(`In the year ${20001}`);
     console.log("hii\nhow r u?");
     console.log("Concatenation" + "works!");
+
 //  4.Boolean
     console.log(5>3)
     console.log(10==5)
     console.log(6>4>1)
     console.log(true||false)
     console.log(true&&false)
+
+// 5.Empty value
+    console.log(null < 0);
+    console.log(null == 0); 
+    console.log(null * 2);
+    console.log(undefined < 0); 
+    console.log(undefined == 0);
 
 
 
