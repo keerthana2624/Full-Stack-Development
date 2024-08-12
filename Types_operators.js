70abbd8ea13e8df1baff9e13b160cd206185275f
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-12 12:22:21 +0530

    implimented the special numbers data types with infinity

--- Types_operators.js
+++ Types_operators.js
@@ -1,8 +1,17 @@
 // Data Types
 // 1.Numbers
-   console.log(2+5);
-   console.log(20 - 30);
-   console.log(1.5+2.0);
-   console.log(8*10);
-   console.log(7/2);
-   console.log(10 % 3);
+    console.log(2+5);
+    console.log(20 - 30);
+    console.log(1.5+2.0);
+    console.log(8*10);
+    console.log(7/2);
+    console.log(10 % 3);
+    
+// 2.Special Numbers
+    // (i)Infinity
+    console.log(10 / 0);    
+    console.log(1000 / 0); 
+    console.log(2e308);     
+    console.log(1e308 * 10);
+    console.log(2e400);
+
