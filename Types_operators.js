a593d41390009fd4cb1860ddd90e382010bcedc7
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-13 14:13:03 +0530

    written the defference blw null and undefined

--- Types_operators.js
+++ Types_operators.js
@@ -4,7 +4,7 @@
     console.log(2+5);
     console.log(20 - 30);
     console.log(1.5+2.0);
-    console.log(8*10);
+    console.log("8"*10);
     console.log(7/2);
     console.log(10 % 3);
     
@@ -93,5 +93,5 @@
     console.log(true + 1);
 
 
-
+ 
 
---
+++ assignment-2.js
@@ -0,0 +1,4 @@
+// 1.Explain the difference between null and undefined in JavaScript.
+/* Null is an empty value and is also a primitive type in JavaScript. The variable which has been assigned as null contains no value.
+   Undefined, means the variable has been declared, but its value has not been assigned.*/
+
