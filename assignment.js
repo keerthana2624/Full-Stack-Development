175ce1a6fabcc084b783ee7860432f5da154029e
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-20 19:22:33 +0530

    implimented the code for countB using forloops in a functions

---
+++ assignment.js
@@ -0,0 +1,13 @@
+function countB(string){
+    count=0
+    for(let i=0;i<string.length;i++){
+        if(string[i]=='B'){
+            count+=1
+        }
+    } 
+    return count
+}
+// console.log(countB("BhaiBhaibhaaB"))
+
+
+   
