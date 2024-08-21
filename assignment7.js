eb64549706f15bccce030bb39061f35b814e6f4f
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-21 11:21:47 +0530

    implimented the code to revers the arrey

--- assignment7.js
+++ assignment7.js
@@ -1,22 +1,24 @@
+// Q1. THE SUM OF RANGE
 
-// function range(start, end, step = 1) {
-//     let array = [];
-//     for (let i = start; i <=end; i+=step) {
-//         array.push(i);
-//     }
-//     return array;
-//     } 
+
+function range(start, end, step = 1) {
+    let array = [];
+    for (let i = start; i <=end; i+=step) {
+        array.push(i);
+    }
+    return array;
+    } 
 // console.log(range(1,10))
     
 
 
-// function range(start, end, step = 1) {
-//     let array = [];
-//     for (let i = end; i >=start; i-=step) {
-//         array.push(i);
-//     }
-//     return array;
-//     } 
+function range(start, end, step = 1) {
+    let array = [];
+    for (let i = end; i >=start; i-=step) {
+        array.push(i);
+    }
+    return array;
+    } 
 // console.log(range(1,10))
 
 
@@ -29,4 +31,17 @@ function rangeaddsum(start,end,step){
     }
     return sum
 }
-console.log(rangeaddsum(1,10,1))
+// console.log(rangeaddsum(1,10,1))
+
+// Q2. REVERSING AN ARRAY
+
+
+function reversArray(array){
+    newarray=[]
+    for(let i=array.length-1;i>=0;i--){
+        newarray.push(array[i])
+    }
+    return newarray
+}
+// console.log(reversArray(["k","e","e","r","t","h","i"]))
+
