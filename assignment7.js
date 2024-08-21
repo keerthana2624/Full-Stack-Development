0a8db35e2c8e888940f1a8369123e9f50c747094
keerthana2624 <keerthanamummy2002@gmail.com>
2024-08-21 10:58:54 +0530

    implimented the code to calculate the sum

--- assignment7.js
+++ assignment7.js
@@ -8,15 +8,25 @@
 //     } 
 // console.log(range(1,10))
     
-function range(start, end, step = 1) {
-    let array = [];
-    for (let i = end; i >=start; i-=step) {
-        array.push(i);
+
+
+// function range(start, end, step = 1) {
+//     let array = [];
+//     for (let i = end; i >=start; i-=step) {
+//         array.push(i);
+//     }
+//     return array;
+//     } 
+// console.log(range(1,10))
+
+
+
+
+function rangeaddsum(start,end,step){
+    sum=0
+    for (let i=start;i<=end;i+=step){
+        sum+=i
     }
-    return array;
-    } 
-console.log(range(1,10))
-
-
-
-
+    return sum
+}
+console.log(rangeaddsum(1,10,1))
