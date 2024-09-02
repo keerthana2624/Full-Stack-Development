f6d3b21fcea7d24486c26f61d794a9be32a8ea40
keerthana2624 <keerthanamummy2002@gmail.com>
2024-09-02 18:56:32 +0530

    implimented the regexp golf and numbers agin

--- asssignment-11.js
+++ asssignment-11.js
@@ -138,3 +138,75 @@ try {
 
 console.log(box.locked); // Check if the box is locked again
 // → true
+
+
+
+
+// Q3. REGEXP GOLF
+// Code golf is a term used for the game of trying to express a particular program in as few characters as
+// possible. Similarly, regexp golf is the practice of writing as tiny a regular expression as possible to match a
+// given pattern and only that pattern.
+// For each of the following items, write a regular expression to test whether the given pattern occurs in a string.
+// The regular expression should match only strings containing the pattern. When your expression works, see
+// whether you can make it any smaller.
+// 1. car and cat
+// 2. pop and prop
+// 3. ferret, ferry, and ferrari
+// 4. Any word ending in ious
+// 5. A whitespace character followed by a period, comma, colon, or semicolon
+// 6. A word longer than six letters
+// 7. A word without the letter e (or E)
+// Refer to the table in the chapter summary for help. Test each solution with a few test strings.
+// // Fill in the regular expressions
+verify(/ca[rt]/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+    verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop", "prrrop"]);
+    verify(/ferr(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+    
+    verify(/\b\w*ious\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+    verify(/\s[.,;:]/,
+    ["bad punctuation ."],
+    ["escape the period"]);
+    verify(/\b\w{7,}\b/,
+    ["Siebentausenddreihundertzweiundzwanzig"],
+    ["no", "three small words"]);
+    verify(/\b[^eE\s]+\b/,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "bedrøvet abe", "BEET"]);
+    function verify(regexp, yes, no) {
+        // Ignore unfinished exercises
+        if (regexp.source == "...") return;
+        for (let str of yes) if (!regexp.test(str)) {
+        console.log(`Failure to match '${str}'`);
+        }
+        for (let str of no) if (regexp.test(str)) {
+        console.log(`Unexpected match for '${str}'`);
+        }
+        }
+// Q4. NUMBERS AGAIN
+    // Write an expression that matches only JavaScript-style numbers. It must support an optional minus or plus sign
+    // in front of the number, the decimal dot, and exponent notation—5e-3 or 1E10—again with an optional sign in
+    // front of the exponent. Also note that it is not necessary for there to be digits in front of or after the dot, but the
+    // number cannot be a dot alone. That is, .5 and 5. are valid JavaScript numbers, but a lone dot isn’t.
+    // Fill in this regular expression.
+    let number = /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/;
+    // Tests:
+    for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+    "1.3e2", "1E-4", "1e+12"]) {
+    if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+    }
+    }
+    for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+    ".5.", "1f5", "."]) {
+    if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+    }
+    }
